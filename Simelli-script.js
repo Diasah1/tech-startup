8489eaa897b7b48058e008056cefbc1a514d807a
simanv <simelliani@gmail.com>
2025-09-24 22:02:45 +0200

    s
    s

---
+++ Simelli-script.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+  document.querySelectorAll('.fill').forEach(bar => {
+    if (bar.classList.contains('html')) {
+      bar.style.width = '90%';
+    } else if (bar.classList.contains('css')) {
+      bar.style.width = '85%';
+    } else if (bar.classList.contains('problemlösning')) {
+      bar.style.width = '90%';
+    } else if (bar.classList.contains('teamwork')) {
+      bar.style.width = '100%';
+    }
+  });
+};
